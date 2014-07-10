b50150ac399aefaf69271b56589ac3f81c69aff0
Andrew Hunter <andrew@logicalshift.co.uk>
2014-07-10 16:40:34 +0100

    Start work on how to represent sprites as objects in the game

---
+++ Sprite/Properties.ts
@@ -0,0 +1,49 @@
+/// <reference path="../RenderQueue/RenderQueue.ts" />
+/// <reference path="../Core/Core.ts" />
+
+module TameGame {
+    /**
+     * The position property describes where a sprite is on screen
+     */
+    export interface IPosition extends Quad {
+        /** Where this sprite should be rendered relative to other sprites */
+        zIndex: number
+    }
+    
+    /**
+     * The position property describes where a sprite is on screen
+     */
+    export var Position: TypeDefinition<IPosition> = {
+        name: createTypeName(),
+        createDefault() {
+            return {
+                x1: 0, y1: 0,
+                x2: 0, y2: 0,
+                x3: 0, y3: 0,
+                x4: 0, y4: 0,
+                
+                zIndex: 0
+            };
+        }
+    };
+    
+    /**
+     * The sprite property describes the sprite that should be displayed by a particular object
+     */
+    export interface ISprite {
+        /** The asset ID of this sprite */
+        assetId: number;
+    }
+    
+    /**
+     * The sprite property describes the sprite that should be displayed by a particular object
+     */
+    export var Sprite: TypeDefinition<ISprite> = {
+        name: createTypeName(),
+        createDefault() {
+            return {
+                assetId: -1
+            };
+        }
+    }
+}
--- TameGame.ts
+++ TameGame.ts
@@ -1,4 +1,5 @@
 /// <reference path="Core/Core.ts"/>
 /// <reference path="RenderQueue/RenderQueue.ts"/>
 /// <reference path="Assets/Assets.ts"/>
+/// <reference path="Sprite/Sprite.ts"/>
 /// <reference path="WebGlRenderer/WebGlRenderer.ts"/>
