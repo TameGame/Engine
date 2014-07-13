a1a524aa0193a53fb42c5c19ffde20e6486a6a1e
Andrew Hunter <andrew@logicalshift.co.uk>
2014-07-13 17:43:56 +0100

    Move the RenderTest into its own script file

---
+++ Test/RenderTest.js
@@ -0,0 +1,37 @@
+var game        = TameGame.game;
+var sprites     = TameGame.sprites;
+
+// Load a sprite
+var rotation    = 0;
+var someSprite  = sprites.loadSprite("TameGame.png");
+
+// Set up the camera every time we get a render
+game.events.onRender(function(queue) {
+    queue.moveCamera(-1, { x:0, y: 0 }, 4.0, rotation);
+});
+
+// Create a sprite object
+var spriteObject = game.createObject();
+spriteObject.get(TameGame.Sprite).assetId = someSprite;
+spriteObject.get(TameGame.Position).set({
+    zIndex: 0,
+    x1: -1, y1: 1,
+    x2: 1,  y2: 1,
+    x3: -1, y3: -1,
+    x4: 1,  y4: -1
+});
+
+// Put it in a scene
+var scene = game.createScene();
+scene.addObject(spriteObject);
+
+game.startScene(scene);
+
+// Update the rotation during the animation pass
+scene.events.onPassStart(function (pass, milliseconds) {
+    if (pass !== TameGame.UpdatePass.Animations) {
+        return;
+    }
+
+    rotation = (milliseconds/10000)*360;
+});
