acd6dc5b56ca0518f79e59fc5137a97f6967dc0a
Andrew Hunter <andrew@logicalshift.co.uk>
2014-07-18 21:41:54 +0100

    Add some simple tests for SAT collisions

---
+++ Test/CollisionTests.js
@@ -0,0 +1,26 @@
+QUnit.test("DoesntCollide", function(assert) {
+    var triangle1 = new TameGame.Polygon([ { x:1, y:1 }, { x:2, y:2 }, { x:1, y:2 }]);
+    var triangle2 = new TameGame.Polygon([ { x:4, y:4 }, { x:5, y:5 }, { x:4, y:5 }]);
+    
+    var collides = TameGame.satCollision(triangle1, triangle2);
+    
+    assert.ok(collides.collided === false);
+});
+
+QUnit.test("DoesCollideWhenOnTopOfEachOther", function(assert) {
+    var triangle1 = new TameGame.Polygon([ { x:1, y:1 }, { x:2, y:2 }, { x:1, y:2 }]);
+    var triangle2 = new TameGame.Polygon([ { x:1, y:1 }, { x:2, y:2 }, { x:1, y:2 }]);
+    
+    var collides = TameGame.satCollision(triangle1, triangle2);
+    
+    assert.ok(collides.collided === true);
+});
+
+QUnit.test("DoesCollideSmallIntersection", function(assert) {
+    var triangle1 = new TameGame.Polygon([ { x:1, y:1 }, { x:2, y:2 }, { x:1, y:2 }]);
+    var triangle2 = new TameGame.Polygon([ { x:1.8, y:1.8 }, { x:2.8, y:2.8 }, { x:1.8, y:2.8 }]);
+    
+    var collides = TameGame.satCollision(triangle1, triangle2);
+    
+    assert.ok(collides.collided === true);
+});
