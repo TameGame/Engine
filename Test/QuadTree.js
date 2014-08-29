03d0528d26724539282160f87001df18fb428ac1
Andrew Hunter <andrew@logicalshift.co.uk>
2014-08-29 10:42:30 +0100

    Check that we can overlap objects safely, plus expand the tree once there are objects in it

--- Test/QuadTree.js
+++ Test/QuadTree.js
@@ -85,3 +85,66 @@ QUnit.test("QuadTreeQuery100Objects", function (assert) {
     assert.ok(count === 100, "100 objects overall");
     if (count !== 100) console.log('100 !== ', count);
 });
+
+QUnit.test("QuadTreeQuery100ObjectsExpandTree", function (assert) {
+    // Create a quadtree with 100 objects in it
+    var tree = new TameGame.QuadTree();
+    populateGrid(tree, 10, 10);
+    var count = 0;
+    
+    // Expand the tree with an object that won't appear in the result
+    tree.addObject({ x: -2, y: -2, width: .5, height: .5 }, {});                // Shouldn't appear in the test; also expands a populated tree    
+    
+    // Should be 9 objects in a 3x3 square
+    tree.forAllInBounds({ x:0, y:0, width:.3, height:.3 }, function () { ++count; });
+    assert.ok(count === 9, "9 objects in 3x3 square");
+    if (count !== 9) console.log('9 !== ', count);
+    
+    // Should be 100 objects overall
+    count = 0;
+    tree.forAllInBounds({ x: 0, y: 0, width:1, height: 1 }, function () { ++count; });
+    assert.ok(count === 100, "100 objects overall");
+    if (count !== 100) console.log('100 !== ', count);
+});
+
+QUnit.test("QuadTreeQuery800Objects", function (assert) {
+    // Create a quadtree with 800 objects in it (overlapped 8 deep)
+    var tree = new TameGame.QuadTree();
+    for (var x=0; x<8; ++x) {
+        populateGrid(tree, 10, 10);
+    }
+    var count = 0;
+    
+    // Should be 9 objects in a 3x3 square
+    tree.forAllInBounds({ x:0, y:0, width:.3, height:.3 }, function () { ++count; });
+    assert.ok(count === 9*8, "9 objects in 3x3 square");
+    if (count !== 9*8) console.log('9*8 !== ', count);
+    
+    // Should be 800 objects overall
+    count = 0;
+    tree.forAllInBounds({ x: 0, y: 0, width:1, height: 1 }, function () { ++count; });
+    assert.ok(count === 800, "800 objects overall");
+    if (count !== 800) console.log('800 !== ', count);
+});
+
+QUnit.test("QuadTreeQuery100ObjectsPlusSomeMore", function (assert) {
+    // Create a quadtree with 100 objects in it
+    var tree = new TameGame.QuadTree();
+    populateGrid(tree, 10, 10);
+    var count = 0;
+    
+    // Add some larger objects to the tree
+    tree.addObject({ x: .1, y: .1, width: .2, height: .2 }, {});                // In the 3x3 square
+    tree.addObject({ x: .5, y: .5, width: .5, height: .5 }, {});                // In the 10x10 square
+    
+    // Should be 9 objects in a 3x3 square
+    tree.forAllInBounds({ x:0, y:0, width:.3, height:.3 }, function () { ++count; });
+    assert.ok(count === 10, "10 objects in 3x3 square");
+    if (count !== 10) console.log('10 !== ', count);
+    
+    // Should be 100 objects overall
+    count = 0;
+    tree.forAllInBounds({ x: 0, y: 0, width:1, height: 1 }, function () { ++count; });
+    assert.ok(count === 102, "102 objects overall");
+    if (count !== 102) console.log('102 !== ', count);
+});
