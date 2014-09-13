151152b55e49058457d11166810ff3f64c6812b7
Andrew Hunter <andrew@logicalshift.co.uk>
2014-09-13 15:12:57 +0100

    Only process the collision between two objects once. Give a way for objects to specify their processing priority.

--- Physics/AabbCollision.ts
+++ Physics/AabbCollision.ts
@@ -6,6 +6,14 @@
 /// <reference path="ShapeCollision.ts" />
 
 module TameGame {
+    // Objects can only collide once per collision pass
+    var collisionPass: number = 0;
+    
+    // Extend the TameObject definition so that it stores the last time this object was in a collision
+    export interface TameObject {
+        lastCollisionPass?: number;
+    }
+    
     /**
      * Object behavior that defines what happens when the axis-aligned bounding box of two objects collide
      *
@@ -26,11 +34,42 @@ module TameGame {
     export var AabbCollisionBehavior: TypeDefinition<IAabbCollisionBehavior> = {
         name: createTypeName(),
         createDefault: () => {
-            return { aabbCollision: (collidedWith, thisObject) => {
+            return { 
+                aabbCollision: (collidedWith, thisObject) => {
+                    // Do no further checking if the object we've collided with has already been processed
+                    // Any collisions that it will have had with this object will already have been taken care of
+                    if (collidedWith.lastCollisionPass === collisionPass) {
+                        return;
+                    }
+                    
+                    // By default, we check if the shapes are collided
                     var collision = areCollided(thisObject, collidedWith);
                 
+                    // If they are, then call appropriate method to indicate that the shapes are colliding
                     if (collision) {
-                        thisObject.getBehavior(ShapeCollisionBehavior).shapeCollision(collision, collidedWith, thisObject);
+                        // Retrieve the reverse collision (in case we need to call both objects)
+                        // TODO: maybe we can just ask the original collision to reverse its perspective for better performance
+                        // This isn't elegant but it will work.
+                        var thatCollision = areCollided(collidedWith, thisObject);
+                        
+                        // Get the behaviour for both objects
+                        var thisCollide = thisObject.getBehavior(ShapeCollisionBehavior);
+                        var thatCollide = collidedWith.getBehavior(ShapeCollisionBehavior);
+                        
+                        // Get the priority for the two objects
+                        var thisPriority = thisCollide.priority?thisCollide.priority(collidedWith):0;
+                        var thatPriority = thatCollide.priority?thatCollide.priority(thisObject):0;
+                        
+                        // Call the behaviours in the appropriate orders
+                        if (thisPriority >= thatPriority) {
+                            if (!thisCollide.shapeCollision(collision, collidedWith, thisObject)) {
+                                thatCollide.shapeCollision(thatCollision, thisObject, collidedWith);
+                            }
+                        } else {
+                            if (!thatCollide.shapeCollision(thatCollision, thisObject, collidedWith)) {
+                                thisCollide.shapeCollision(collision, collidedWith, thisObject);
+                            }
+                        }
                     }
                 }
             };
@@ -41,6 +80,12 @@ module TameGame {
      * When an object moves, test for collision with other nearby objects
      */
     export function generateAabbCollisionBehavior(game: Game) {
+        // Every update pass, update the collision pass number
+        game.events.onPassStart(UpdatePass.Preparation, () => {
+            ++collisionPass;
+        });
+        
+        // When the presence for an object is updated, check for collisions and react if necessary
         game.watch(Presence, UpdatePass.PhysicsCollision, (obj, presence) => {
             // Get the object's location
             var aabb    = obj.aabb;
@@ -61,6 +106,9 @@ module TameGame {
                 // Run the collision behavior
                 obj.getBehavior(AabbCollisionBehavior).aabbCollision(collideObj, obj);
             });
+            
+            // Update the collision pass number of this object
+            obj.lastCollisionPass = collisionPass;
         });
     }
 }
--- Physics/ShapeCollision.ts
+++ Physics/ShapeCollision.ts
@@ -22,17 +22,32 @@ module TameGame {
      * Behaviour that describes what happens when two object's shapes collide
      */
     export interface IShapeCollisionBehavior {
+        /**
+         * Optional function that can be used to supply a collision priority
+         *
+         * When two objects collide, shapeCollision is called for them both. If they have the
+         * same priority then the order is arbitrary. Additionally, if one object returns true
+         * to indicate that the collision has been handled, then the other object is not
+         * consulted.
+         *
+         * Objects that return a higher value are processed first. The function can optionally
+         * look at the object being collided with to change priorities.
+         */
+        priority?: (collidedWith: TameObject) => number;
+        
         /** 
          * This object has collided with another
+         *
+         * This should return true if the collision has been handled, or false if it has not
          */
-        shapeCollision(collision: Collision, withObject: TameObject, thisObject: TameObject);
+        shapeCollision(collision: Collision, withObject: TameObject, thisObject: TameObject): boolean;
     }
     
     export var ShapeCollisionBehavior: TypeDefinition<IShapeCollisionBehavior> = {
         name: createTypeName(),
         
         createDefault: () => {
-            return { shapeCollision: () => {} };
+            return { shapeCollision: () => { return false; } };
         }
     }
 }
