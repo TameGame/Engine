e3e40d74cd75e5f9763890d80d2196ed206b4dd9
Andrew Hunter <andrew@logicalshift.co.uk>
2014-09-13 15:33:10 +0100

    Add a shape to the test object in the render test

--- Test/RenderTest.js
+++ Test/RenderTest.js
@@ -7,15 +7,24 @@ var spriteIds   = TameGame.loadTpJsonSpriteSheet(sprites, data, 'Sprites/SpriteS
     var someSprite  = spriteIds['TameGame.png'];
 
     // Create a sprite object
-    var spriteObject = game.createObject();
-    spriteObject.get(TameGame.Sprite).assetId = someSprite;
-    spriteObject.get(TameGame.Position).set({
-        zIndex: 0,
-        x1: -1, y1: 1,
-        x2: 1,  y2: 1,
-        x3: -1, y3: -1,
-        x4: 1,  y4: -1
-    });
+    function createSprite() {
+        var spriteObject = game.createObject();
+        spriteObject.get(TameGame.Sprite).assetId = someSprite;
+        spriteObject.get(TameGame.Position).set({
+            zIndex: 0,
+            x1: -1, y1: 1,
+            x2: 1,  y2: 1,
+            x3: -1, y3: -1,
+            x4: 1,  y4: -1
+        });
+        
+        var spriteShape = new TameGame.Polygon([ { x:-1, y:1 }, { x:1, y:1 }, { x:1,y:-1 }, { x:-1,y:-1 } ]);
+        spriteObject.get(TameGame.Presence).shape = spriteShape;
+        
+        return spriteObject;
+    }
+    
+    var spriteObject = createSprite();
     
     // Rotate at 20 degress/second
     spriteObject.get(TameGame.Motion).rotationVelocity = 20.0;
