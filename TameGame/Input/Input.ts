abc8e0e8c25d1132ae67ce53b007df027cc64582
Andrew Hunter <andrew@logicalshift.co.uk>
2014-12-07 11:31:25 +0000

    Move the default control router into a class of its own

--- TameGame/Input/Input.ts
+++ TameGame/Input/Input.ts
@@ -1,4 +1,5 @@
 /// <reference path="Interface.ts" />
 /// <reference path="KeyboardInput.ts" />
 /// <reference path="Dispatch.ts" />
+/// <reference path="DefaultControlRouter.ts" />
 /// <reference path="GameInput.ts" />
