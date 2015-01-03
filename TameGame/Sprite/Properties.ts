e0d48b2729a114226d764f759f8cfdc5d2790ca7
Andrew Hunter <andrew@logicalshift.co.uk>
2015-01-03 21:42:54 +0000

    Fix some broken formatting

--- TameGame/Sprite/Properties.ts
+++ TameGame/Sprite/Properties.ts
@@ -18,14 +18,14 @@ module TameGame {
      * The position property describes where a sprite is on screen
      */
     export var Position: TypeDefinition<IPosition> = declareProperty("position", () => {
-            return {
-                x1: 0, y1: 0,
-                x2: 0, y2: 0,
-                x3: 0, y3: 0,
-                x4: 0, y4: 0,
-                
-                zIndex: 0
-            };
+        return {
+            x1: 0, y1: 0,
+            x2: 0, y2: 0,
+            x3: 0, y3: 0,
+            x4: 0, y4: 0,
+            
+            zIndex: 0
+        };
     });
             
     /**
