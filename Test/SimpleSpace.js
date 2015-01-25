da2cf01bc1e8ca914bdcffc7f28e45f14913a15f
Andrew Hunter <andrew@logicalshift.co.uk>
2015-01-25 11:50:00 +0000

    Fix tests

--- Test/SimpleSpace.js
+++ Test/SimpleSpace.js
@@ -1,6 +1,22 @@
+function makeSpaceLocation(bounds) {
+    return { 
+        tile: { 
+            quad: TameGame.bbToQuad(bounds),
+            zIndex: 0
+        },
+        presence: {
+            shape: null
+        },
+        location: {
+            pos: { x: 0, y: 0 },
+            angle: 0
+        }
+    }
+}
+
 QUnit.test("SimpleSpaceCanAddObject", function (assert) {
     var tree    = new TameGame.SimpleSpace();
-    var someRef = tree.addObject({}, { x: 0.5, y: 0.5, width: 0.25, height: 0.25 });
+    var someRef = tree.addObject({}, makeSpaceLocation({ x: 0.5, y: 0.5, width: 0.25, height: 0.25 }));
     
     assert.ok(someRef !== null);
 });
@@ -8,7 +24,7 @@ QUnit.test("SimpleSpaceCanAddObject", function (assert) {
 
 QUnit.test("SimpleSpaceCanRemoveObject", function (assert) {
     var tree    = new TameGame.SimpleSpace();
-    var someRef = tree.addObject({}, { x: 0.5, y: 0.5, width: 0.25, height: 0.25 });
+    var someRef = tree.addObject({}, makeSpaceLocation({ x: 0.5, y: 0.5, width: 0.25, height: 0.25 }));
     var count;
 
     count = 0;
@@ -24,7 +40,7 @@ QUnit.test("SimpleSpaceCanRemoveObject", function (assert) {
 
 QUnit.test("SimpleSpaceCanFindObject", function (assert) {
     var tree    = new TameGame.SimpleSpace();
-    var someRef = tree.addObject({}, { x: 0.5, y: 0.5, width: 0.25, height: 0.25 });
+    var someRef = tree.addObject({}, makeSpaceLocation({ x: 0.5, y: 0.5, width: 0.25, height: 0.25 }));
     var count   = 0;
     
     tree.forAllInBounds({ x:0, y: 0, width: 1, height: 1 }, function (obj) { ++count });
@@ -35,7 +51,7 @@ QUnit.test("SimpleSpaceCanFindObject", function (assert) {
 
 QUnit.test("SimpleSpaceCanFindHugeObject", function (assert) {
     var tree    = new TameGame.SimpleSpace();
-    var someRef = tree.addObject({}, { x: -100, y: -100, width: 200, height: 200 });            // Forces the space to expand outward
+    var someRef = tree.addObject({}, makeSpaceLocation({ x: -100, y: -100, width: 200, height: 200 }));            // Forces the space to expand outward
     var count   = 0;
     
     tree.forAllInBounds({ x:-2, y: -2, width: 4, height: 4 }, function (obj) { ++count });
@@ -46,7 +62,7 @@ QUnit.test("SimpleSpaceCanFindHugeObject", function (assert) {
 
 QUnit.test("SimpleSpaceExcludeObjectOutOfBounds", function (assert) {
     var tree    = new TameGame.SimpleSpace();
-    var someRef = tree.addObject({}, { x: 0.5, y: 0.5, width: 0.25, height: 0.25 });
+    var someRef = tree.addObject({}, makeSpaceLocation({ x: 0.5, y: 0.5, width: 0.25, height: 0.25 }));
     var count   = 0;
     
     tree.forAllInBounds({ x:-1, y: -1, width: 1, height: 1 }, function (obj) { 
@@ -70,7 +86,7 @@ function populateGrid(space, width, height) {
     for (y=0; y<height; ++y) {
         xpos = 0;
         for (x=0; x<width; ++x) {
-            space.addObject({ x: xpos, y: ypos }, { x: xpos, y: ypos, width: distX, height: distY });
+            space.addObject({ x: xpos, y: ypos }, makeSpaceLocation({ x: xpos, y: ypos, width: distX, height: distY }));
             
             xpos += distX;
         }
@@ -104,7 +120,7 @@ QUnit.test("SimpleSpaceQuery100ObjectsExpandTree", function (assert) {
     var count = 0;
     
     // Expand the tree with an object that won't appear in the result
-    tree.addObject({}, { x: -2, y: -2, width: .5, height: .5 });                // Shouldn't appear in the test; also expands a populated tree    
+    tree.addObject({}, makeSpaceLocation({ x: -2, y: -2, width: .5, height: .5 }));                // Shouldn't appear in the test; also expands a populated tree    
     
     // Should be 9 objects in a 3x3 square
     tree.forAllInBounds({ x:0, y:0, width:.3, height:.3 }, function () { ++count; });
@@ -145,8 +161,8 @@ QUnit.test("SimpleSpaceQuery100ObjectsPlusSomeMore", function (assert) {
     var count = 0;
     
     // Add some larger objects to the tree
-    tree.addObject({}, { x: .1, y: .1, width: .2, height: .2 });                // In the 3x3 square
-    tree.addObject({}, { x: .5, y: .5, width: .5, height: .5 });                // In the 10x10 square
+    tree.addObject({}, makeSpaceLocation({ x: .1, y: .1, width: .2, height: .2 }));                // In the 3x3 square
+    tree.addObject({}, makeSpaceLocation({ x: .5, y: .5, width: .5, height: .5 }));                // In the 10x10 square
     
     // Should be 9 objects in a 3x3 square
     tree.forAllInBounds({ x:0, y:0, width:.3, height:.3 }, function () { ++count; });
